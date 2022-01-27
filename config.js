adc0549a79d9a879a860ebd8d748e9a632d0c2ae
krishnajana <76243764+krishnajana@users.noreply.github.com>
2022-01-27 15:38:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+import { initializeApp } from "firebase/app";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCSuW-zaHRiCAI7LbpXlqg99MZmXYtHfx8",
+    authDomain: "project-67-7b8fa.firebaseapp.com",
+    databaseURL: "https://project-67-7b8fa-default-rtdb.firebaseio.com",
+    projectId: "project-67-7b8fa",
+    storageBucket: "project-67-7b8fa.appspot.com",
+    messagingSenderId: "921249473332",
+    appId: "1:921249473332:web:48c66ec40b3665deb9bb61"
+  }
+
+  const app = initializeApp(firebaseConfig);
